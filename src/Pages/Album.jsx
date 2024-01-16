d581a38ef0f0ab2b650c5d2caf786a8690e63365
guitarbuju <gerardoamadrid@gmail.com>
2024-01-16 21:06:53 +0100

    fixed album and cards on home base responsive

--- src/Pages/Album.jsx
+++ src/Pages/Album.jsx
@@ -11,7 +11,7 @@ const { selectedDeck }= useAlbumContext()
   return (
     <div>
 
-      <ul className="grid grid-cols-5 gap-4">
+      <ul className="grid grid-cols-2 sm:grid-cols-5 gap-4 mt-40 sm:mt-0">
         {selectedDeck.map((card, index) => (
           <li className="col-span-1 w-full" key={index}  style={{ animationDelay: `${index * 0.1}s` }}>
             <Card2 img={card.img} name={card.name} id={card.id} face={card.face}/>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,16 +4,16 @@
 
 const Card = ({img, name, id }) => {
   return (
-    <div className=" border-8 border-double border-gray-200 max-w-xs p-6 rounded-xl shadow-2xl bg-gray-900 text-yellow-400">
+    <div className=" border-8 border-double border-gray-200  p-6 rounded-xl shadow-2xl bg-gray-900 text-yellow-400">
      <span className="">{id}</span>
      
-      <img src={img} alt=""className="object-cover object-center w-full rounded-md h-52 dark:bg-gray-500" />
+      <img src={img} className="object-cover object-center w-full h-44 rounded-md sm:h-52 dark:bg-gray-500" />
       
       
 	
-	<div className="mt-6 mb-2">
+	<div className="mt-6 mb-2 w-full">
    
-		<h2 className="text-xl font-semibold tracki truncate max-w-xs">{name}</h2>
+		<h2 className="sm:text-xl font-semibold tracki truncate  text-wrap">{name}</h2>
 	</div>
 	
 </div>
