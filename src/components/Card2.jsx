a8bf08de2266c0c6fa4a3c3fe0ddf7b471eac7da
guitarbuju <gerardoamadrid@gmail.com>
2024-01-19 21:24:30 +0100

    responsive complete

--- src/components/Card2.jsx
+++ src/components/Card2.jsx
@@ -1,29 +1,38 @@
 /* eslint-disable react/prop-types */
 
-
-
-const Card = ({img, name, id , face}) => {
+const Card = ({ img, name, id, face }) => {
   return (
-    <div className=" border-8 border-double  border-gray-200 max-w-xs p-6 rounded-xl shadow-2xl bg-gray-900 text-yellow-400">
-     <span className="">{id}</span>
-     <div>
-     {face ? <img src={img} alt=""
-      className="object-cover object-center w-full rounded-md h-20 sm:h-52" />
-      :
-      <img src={img} alt=""
-      className="object-cover object-center w-full rounded-md h-20 sm:h-52  opacity-5" />}
-      
-     </div>
-	
-	<div className="mt-6 mb-2">
-   {face ? <h2 className="text-xl font-semibold tracki truncate max-w-xs">{name}</h2>
-   : 
-   <h2 className=" text-gray-600 text-xl font-semibold tracki truncate max-w-xs">{name}</h2>}
-		
-	</div>
-	
-</div>
-  )
-}
+    <div className=" border-8 border-double  border-gray-200  p-6 rounded-xl shadow-2xl bg-gray-900 text-yellow-400">
+      <span className="sm:text-sm">{id}</span>
+      <div className="flex justify-center align-middle sm:h-28">
+        {face ? (
+          <img
+            src={img}
+            alt=""
+            className="object-cover object-center w-full rounded-md h-40 sm:h-40 sm:w-52"
+          />
+        ) : (
+          <img
+            src={img}
+            alt=""
+            className="object-cover object-center w-full rounded-md h-40 sm:h-40 sm:w-52 opacity-5"
+          />
+        )}
+      </div>
 
-export default Card
+      <div className="mt-6 mb-2">
+        {face ? (
+          <h2 className="text-xl sm:text-sm font-semibold tracki truncate max-w-xs">
+            {name}
+          </h2>
+        ) : (
+          <h2 className=" text-gray-600 text-xl sm:text-sm font-semibold tracki truncate max-w-xs">
+            {name}
+          </h2>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
